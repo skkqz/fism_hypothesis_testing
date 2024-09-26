9eadd12bbb3131af9dd4a3ffd92a91a82a0bcc36
Andrew <skkqw@yandex.ru>
2024-09-26 22:32:50 +0300

    Добавлены сигналы для создания профиля

--- server/accounts/apps.py
+++ server/accounts/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AcountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        import accounts.signals
